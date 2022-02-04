71933f0d559dae1a5ac42581cbc6c17bf8e9a41c
Derry Bradley <derrybradley@live.ie>
2022-02-04 12:32:07 +0000

    Update elevator

--- src/Elevator.js
+++ src/Elevator.js
@@ -14,14 +14,14 @@ class Elevator extends Component {
                     <p id="pone">
                         How long does it take for your company to generate business value from a decision?
                         
-                        Two vital metrics for any project are Success Rate and Mean Time to Recovery, and using CircleCI’s platform you can target these to reduce End-to-end build time by up to 50% by utilizing features such as Test Splitting, caching and docker native executors
-                    </p>
-                    <p id="pone"> 
-                        We pride ourselves on being the missing link between what Engineers are good at which is ideating and creating awesome applications and what Computers are good at - automation.
-
 
                         As an industry leader in CI/CD, we enable our customers to operate above the value line by making Software for organisations and individuals an idea problem, not a delivery problem.
+                        
+                        We pride ourselves on being the missing link between what Engineers are good at which is ideating and creating awesome applications and what Computers are good at - automation.
 
+                    </p>
+                    <p id="pone"> 
+                        Two vital metrics for any project are Success Rate and Mean Time to Recovery, and using CircleCI’s platform you can target these to reduce End-to-end build time by up to 50% by utilizing features such as Test Splitting, caching and docker native executors
 
                     </p>
                     <p id="pone">
